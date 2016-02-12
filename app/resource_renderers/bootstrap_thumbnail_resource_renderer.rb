b578ec6f0536dd8603883d777bf79009824b3b16
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-02-12 18:31:35 +0100

    Fixed thumbnail size.

--- app/resource_renderers/bootstrap_thumbnail_resource_renderer.rb
+++ app/resource_renderers/bootstrap_thumbnail_resource_renderer.rb
@@ -1,12 +1,12 @@
 class BootstrapThumbnailResourceRenderer < ResourceRenderer::ResourceRenderer::Base
     def render(&block)
-    helper.content_tag(:div) do
+    helper.content_tag(:div, { :'data-target' => '#modal-gallery', :'data-toggle' => 'modal-gallery'}) do
       block.call(self)
     end
   end
 
   def thumbnail(attribute_name, options = {}, &block)
-    options[:as] = :thumbnail
+    options.reverse_merge!(as: :thumbnail, image_tag_options: { class: 'thumbnail img-responsive' })
 
     helper.content_tag(:div, class: 'col-xs-12 col-sm-6 col-md-4 col-lg-2') do
       display(attribute_name, options, &block)
