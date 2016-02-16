9278f1825b44c5578bd38d6ef7732c2520081626
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-02-16 18:13:04 +0100

    Added the possiblity to set the title.

--- app/resource_renderers/bootstrap_media_object_resource_renderer.rb
+++ app/resource_renderers/bootstrap_media_object_resource_renderer.rb
@@ -1,6 +1,7 @@
 class BootstrapMediaObjectResourceRenderer < ResourceRenderer::ResourceRenderer::Base
   def media_object(options = {})
-    options.reverse_merge!(title_method: :title, body_method: :body, image_method: :image, image_url_method: :url, body: nil)
+    options.reverse_merge!(title_method: :title, body_method: :body, image_method: :image, image_url_method: :url, body: nil, title: nil)
+    title            = options.delete(:title)
     title_method     = options.delete(:title_method)
     body_method      = options.delete(:body_method)
     body             = options.delete(:body)
@@ -9,7 +10,7 @@ class BootstrapMediaObjectResourceRenderer < ResourceRenderer::ResourceRenderer:
     image_url        = options.delete(:image_url)
 
     locals = {
-      title: @resource.send(title_method),
+      title: title || @resource.send(title_method),
       body:  body || @resource.send(body_method),
       image_src: image_url || @resource.send(image_method).send(image_url_method),
       link_href: helper.url_for(@resource)
