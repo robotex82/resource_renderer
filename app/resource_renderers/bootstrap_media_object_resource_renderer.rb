c73b6befa01b0509d4eb38d7bd21821cfddb5079
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-03-10 11:57:55 +0100

    Added link_url optiont to bootstrap media object resource renderer.

--- app/resource_renderers/bootstrap_media_object_resource_renderer.rb
+++ app/resource_renderers/bootstrap_media_object_resource_renderer.rb
@@ -7,7 +7,8 @@ class BootstrapMediaObjectResourceRenderer < ResourceRenderer::ResourceRenderer:
       image_url_method: :url,
       body: nil,
       title: nil,
-      media_body_content_html_options: { class: 'media-body-content.bottom-margin-1' }
+      media_body_content_html_options: { class: 'media-body-content.bottom-margin-1' },
+      link_url: nil
     )
     title            = options.delete(:title)
     title_method     = options.delete(:title_method)
@@ -17,13 +18,14 @@ class BootstrapMediaObjectResourceRenderer < ResourceRenderer::ResourceRenderer:
     image_url_method = options.delete(:image_url_method)
     image_url        = options.delete(:image_url)
     link_to_more_url = options.delete(:link_to_more_url)
+    link_url         = options.delete(:link_url)
     media_body_content_html_options = options.delete(:media_body_content_html_options)
 
     locals = {
       title:            title || @resource.send(title_method),
       body:             body || @resource.send(body_method),
       image_src:        image_url || @resource.send(image_method).send(image_url_method),
-      link_url:         link_to_more_url || helper.url_for(@resource),
+      link_url:         link_url || helper.url_for(@resource),
       link_to_more_url: link_to_more_url,
       media_body_content_html_options: media_body_content_html_options
     }
